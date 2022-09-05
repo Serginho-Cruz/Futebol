3f2018dd5542339b316232624a770d375bafc039
RobertoBronco <roberto.t.alves@gmail.com>
2022-09-05 15:10:59 -0300

    fix-path teste

--- lib/src/data/usecases/get_selection_impl.dart
+++ lib/src/data/usecases/get_selection_impl.dart
@@ -1,8 +1,9 @@
-import 'package:futebol/src/domain/entities/selecao_entity.dart';
 import 'package:dartz/dartz.dart';
-import 'package:futebol/src/domain/usecases/getSelection/get_selection_by_id_interface.dart';
-import 'package:futebol/src/errors/errors_mensages_classes/errors_mensages.dart';
+
+import '../../domain/entities/selecao_entity.dart';
+import '../../domain/usecases/getSelection/get_selection_by_id_interface.dart';
 import '../../errors/errors_classes/errors_classes.dart';
+import '../../errors/errors_mensages_classes/errors_mensages.dart';
 import '../repository/repository_interface.dart';
 
 class GetSelectionUC implements IGetSelection {
